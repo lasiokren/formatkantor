5ff931b213ea260516dede2b2d3142157bf43297
lasiokren <lasiokren@gmail.com>
2025-12-03 12:41:46 +0700

    Add Pegawai model with id, nama, and nip fields

--- app/models/models.py
+++ app/models/models.py
@@ -1,1 +1,10 @@
+from sqlalchemy import Column, Integer, String
+from ..database import Base
+
+class Pegawai(Base):
+    __tablename__ = "pegawai"
+
+    id = Column(Integer, primary_key=True, index=True)
+    nama = Column(String)
+    nip = Column(String)
 
